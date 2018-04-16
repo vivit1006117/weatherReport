6c201bd8f265b66bf90baeecb202c6361fec4549
Vivek Kumar <kvivek@thoughtworks.com>
2018-04-16 17:46:58 +0530

    making caching work for apis

--- frontend/src/app/app.service.ts
+++ frontend/src/app/app.service.ts
@@ -1,21 +1,32 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 import 'rxjs/add/operator/map';
-import {promise} from "selenium-webdriver";
 import {Observable} from "rxjs/Observable";
-import {PromiseObservable} from "rxjs/observable/PromiseObservable";
+import "rxjs/add/operator/publishReplay";
 
 @Injectable()
 export class WeatherForecastService {
+  private forecast = new Map();
+  private forecastDetails = new Map();
   constructor (
     private http: HttpClient
   ) {}
 
   getForecast(id: number): Observable<Object> {
-    return this.http.get(`http://api.openweathermap.org/data/2.5/weather?id=${id}&APPID=515fe162ca34547fcfc9cc438828f8fe`);
+    if (!this.forecast[id]) {
+      this.forecast[id] = this.http.get(`http://api.openweathermap.org/data/2.5/weather?id=${id}&APPID=515fe162ca34547fcfc9cc438828f8fe`)
+        .publishReplay(1)
+        .refCount();
+    }
+    return this.forecast[id];
   }
 
   getDetailForecast(selectedId: number): Observable<Object> {
-    return this.http.get(`http://api.openweathermap.org/data/2.5/forecast?id=${selectedId}&APPID=515fe162ca34547fcfc9cc438828f8fe`);
+    if (!this.forecastDetails[selectedId]) {
+      this.forecastDetails[selectedId] = this.http.get(`http://api.openweathermap.org/data/2.5/forecast?id=${selectedId}&APPID=515fe162ca34547fcfc9cc438828f8fe`)
+        .publishReplay(1)
+        .refCount();
+    }
+    return this.forecastDetails[selectedId];
   }
 }
